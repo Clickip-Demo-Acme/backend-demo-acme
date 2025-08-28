d51ec0db39a99e3ecaf3123f0d7c79f150cee7f9
josewerlich <werlich@outlook.com>
2025-08-28 11:10:11 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello from Backend Demo API!");
+app.MapControllers();
+
+app.Run();
